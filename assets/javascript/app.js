91db36395983539b2ea2b4fd0a365d9def6d0e58
joshroy326 <joshroy326@gmail.com>
2019-07-08 18:28:54 -0500

    added some psuedo code and will start again tomorrow

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// need to create a start button for the user to hit and then the page will reveal the timer and the questions
+
+// create a timer when the page loads so the user knows how much time he has left to answer the questions given
+
+
+//need to create logic when the timer ends that the user is alerted that the time is up and that its time to check the answers
+
+
+//need to make sure this is a way that the user only gets to answere one option
+
+
+// when the timer ends the page will need to transition to a new window or section or some hidden content that will only show when the timer ends and will show the user how many answers hes gotten correct or incorrect
