c4d89bc4e1d8ab602f5e34041e73fb200abcc015
joshroy326 <joshroy326@gmail.com>
2019-07-11 14:19:50 -0500

    added functions when to hide questions at the start of the game and when the user clicks start the questions will appear and the timer will start the coutdown still need to add the logic to when the timer hits 0 that it wont go below 0 and for the next page of content to appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,58 @@
 // need to create a start button for the user to hit and then the page will reveal the timer and the questions
 
+
+//  Variable that will hold our interval ID when we execute
+    //  the "run" function
+var intervalId;
+var clockRunning = false;
+var time = 30;
+
+$(".clock-display").text("00:00");
+
+window.onload = function(){
+    $("#start-button").on("click",startGame);
+    $('#start-button').on("click",startTimer);
+    return hideQuestions();
+    
+}
+function showQuestions(){
+    $('.question').show();
+    $('.clock-display').show();
+}
+function hideQuestions(){
+    $('.question').hide();
+    $('.clock-display').hide();
+}
+function startGame() {
+    $('#start-container').hide();
+    console.log("timer has been started and game has been started")
+    return showQuestions();
+ 
+}
+
+//when start button is clicked it will call the start timer fucntion
+
+
+function startTimer(){
+    if (!clockRunning) {
+        intervalId = setInterval(decrement, 1000);
+        clockRunning = true;
+      }
+    console.log("i have been clicked and running this fucntiion");
+    return decrement();
+
+    
+}
+
+function decrement(){
+    time--;
+    $(".clock-display").text("Seconds Left: " + time);
+}
+
+//create where when you start game it starts the timer and need to creat a fucntion that will run the timer
+
 // create a timer when the page loads so the user knows how much time he has left to answer the questions given
+
 
 
 //need to create logic when the timer ends that the user is alerted that the time is up and that its time to check the answers
